db648119f0c293d41e1e345e2038e171161f19b0
Abdulrhman Rabea <abdorabee181@gmail.com>
2025-11-23 00:36:24 +0200

    Remove dashboard loading redirect
    Remove redirect logic for dashboard loading.

--- frontend-react-next/proxy.ts
+++ frontend-react-next/proxy.ts
@@ -5,10 +5,7 @@ import type { NextRequest } from "next/server";
 const HostIP = process.env.auth_local_ip;
 
 export function proxy(req: NextRequest) {
-    //to stop loading dashboard until refactor it 
-    if (req.nextUrl.pathname.startsWith("/dashboard")) {
-        return NextResponse.redirect(new URL("/", req.url));
-    }
+   
     const refreshToken = req.cookies.get("refreshToken")?.value;
 
     if (!refreshToken) {
