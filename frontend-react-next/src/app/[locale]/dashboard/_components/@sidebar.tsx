dd856aaf80ab97cdd3230b06d097ecc96525fbbe
Mustafa <mm0118250@gmail.com>
2025-12-03 22:53:12 +0200

    adding logout button to sidebar in the dashboard

--- frontend-react-next/src/app/[locale]/dashboard/_components/@sidebar.tsx
+++ frontend-react-next/src/app/[locale]/dashboard/_components/@sidebar.tsx
@@ -12,8 +12,10 @@ import {
   ListOrdered,
   LayoutDashboard,
   CalendarDaysIcon,
-  ChartArea
+  ChartArea,
+  LogOut
 } from "lucide-react";
+import { logoutRequest } from "@/services/auth";
 
 const ICONS = {
   LayoutDashboard,
@@ -22,7 +24,8 @@ const ICONS = {
   CalendarDaysIcon,
   Bell,
   ListOrdered,
-  ChartArea
+  ChartArea,
+  LogOut
 };
 
 const links = [
@@ -46,8 +49,9 @@ const Sidebar = () => {
       <motion.div
         animate={{ width: isOpen ? 240 : 70 }}
         transition={{ duration: 0.01 }}
-        className={`flex flex-col min-h-screen bg-gray-900 text-white shadow-lg transition-all duration-300 ${isOpen ? "z-50" : ""
-          }`}
+        className={`flex flex-col min-h-screen bg-gray-900 text-white shadow-lg transition-all duration-300 ${
+          isOpen ? "z-50" : ""
+        }`}
       >
         {/* Header */}
         <div className="flex items-center justify-between p-4 border-b border-gray-700">
@@ -70,10 +74,11 @@ const Sidebar = () => {
               <Link
                 key={link.name}
                 href={link.to}
-                className={`flex items-center gap-3 px-3 py-2 rounded-lg transition-all duration-200 ${isActive
+                className={`flex items-center gap-3 px-3 py-2 rounded-lg transition-all duration-200 ${
+                  isActive
                     ? "bg-linear-to-r from-[#a07916] via-[#E4B441] to-[#a07916] shadow-[0_0_10px_#E4B44180]"
                     : "text-gray-300 hover:bg-gray-800 hover:text-white"
-                  }`}
+                }`}
               >
                 <Icon className="w-5 h-5 shrink-0" />
                 {isOpen && <span className="font-medium">{link.name}</span>}
@@ -81,7 +86,16 @@ const Sidebar = () => {
             );
           })}
         </nav>
-
+        {/* logout button */}
+        <div className="p-3 border-t border-gray-700">
+          <button
+            onClick={logoutRequest}
+            className="flex items-center gap-3 px-3 py-2 text-gray-300 hover:bg-red-800 hover:text-white rounded-lg transition-all duration-200 w-full text-left"
+          >
+            <LogOut className="w-5 h-5 shrink-0" />
+            {isOpen && <span className="font-medium">Logout</span>}
+          </button>
+        </div>
         {/* Footer */}
         <div className="p-3 border-t border-gray-700 text-sm text-gray-400">
           {isOpen ? "© 2025 MyApp" : "©"}
